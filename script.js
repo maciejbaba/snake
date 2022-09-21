e9263256fc47d529e9aa1be9da6a4c0fef5f30ae
maciek5000 <maciek2baba2@gmail.com>
2022-09-21 21:01:06 +0200

    create function that draws apple, draw snake

--- script.js
+++ script.js
@@ -6,7 +6,29 @@ const numberOfBlocks = SIZE_OF_BOARD / SIZE_OF_BLOCK
 const widthInBlocks = Math.sqrt(numberOfBlocks)
 const heightInBlocks = Math.sqrt(numberOfBlocks)
 
+let isApple = false
+
+document.addEventListener("keydown", keyPushed)
+
+let snake = [{
+  x:7,
+  y:7
+},{
+  x:6,
+  y:7
+},{
+  x:5,
+  y:7
+}]
+
 createGameBoard()
+
+function keyPushed(event) {
+  switch(event) {
+    case 37:
+      
+  }
+}
 
 function createGameBoard() {
   gameBoard.style.setProperty("--board-width", widthInBlocks)
@@ -18,6 +40,26 @@ function createGameBoard() {
   }
 }
 
-function createSnake() {
+function createApple() {
+  if (isApple) return
+  let x = Math.floor(Math.random() * widthInBlocks)
+  let y = Math.floor(Math.random() * heightInBlocks)
 
+  let appleBlockId = x + " " + y
+  let appleBlock = document.getElementById(appleBlockId)
+  if (appleBlock.style.backgroundColor == "white") {
+    // add one to snake if renders inside
+  }
+  appleBlock.style.backgroundColor = "red"
+}
+
+drawSnake()
+createApple()
+
+function createSnake() {
+  snake.forEach(bodyPart => {
+    let idOfBlock = bodyPart.y + " " + bodyPart.x
+    let blockUnderBodyPart = document.getElementById(idOfBlock)
+    blockUnderBodyPart.style.backgroundColor = "white"
+  })
 }
